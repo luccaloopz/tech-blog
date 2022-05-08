403346324a863300fb9c1a7a5be47a7043f1e01d
luccaloopz <luccamartins2012@gmail.com>
2022-05-07 21:23:31 -0700

    wrote routes for homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,8 +3,71 @@ const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 const sequelize = require('../config/connection');
 
-// this is where I'll write the homeRoutes
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        }); 
 
+        const posts = postData.map((post) => post.get({ plain: true }));
 
+        res.render('homepage', {
+            posts,
+            logged_in: req.session.logged_in
+        });
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    };
+    
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    if(req.session.logged_in) {
+        res.redirect('/');
+        return;
+    };
+
+    res.render('signup');
+});
+
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        });
+
+        if (!postData) {
+            res.status(404).json({ message: "No post found with this id!"});
+            return;
+        } else {
+            const posts = postData.map((post) => post.get({ plain: true }));
+
+            res.render('one-post', {
+                posts,
+                logged_in: req.session.logged_in
+            })
+        };
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
