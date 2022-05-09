16e8f5b87c790ce491270cb18f06d6c0f73c2dd1
luccaloopz <luccamartins2012@gmail.com>
2022-05-09 01:31:53 -0700

    wrote user api get all users, get user by id, and create new user. Also, delete user email as criteria.

--- seeds/userData.js
+++ seeds/userData.js
@@ -3,22 +3,18 @@ const { User } = require('../models');
 const users = [
     {
         'username': 'john123',
-        'email': 'johndoe@gmail.com',
         'password': 'password1' 
     },
     {
         'username': 'Sam234',
-        'email': 'samdoe@gmail.com',
         'password': 'password2'
     },
     {
         'username': 'Sally345',
-        'email': 'sallydoe@gmail.com',
         'password': 'password3'
     },
     {
         'username': 'Jane456',
-        'email': 'janedoe@gmail.com',
         'password': 'password4'
     }
 ];
