4a6d369c36fcafd3e62d0b6c332319cfe9bcd987
luccaloopz <luccamartins2012@gmail.com>
2022-05-08 19:26:42 -0700

    create the views for what the user sees on the dashboard as well as wrote the route for when a user clicks on a post in their dashboard, which brings them to an edit form for their post.

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -3,8 +3,47 @@ const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 const sequelize = require('../config/connection');
 
-// this is where I'll write the dashboardRoutes
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            where: { user_id: req.session.user_id },
+            include: {
+                model: Comment,
+                include: { model: User }
+            }
+        });
 
+        const posts = postData.map((post) => post.get({ plain: true }));
 
+        res.render('dashboard', {
+            posts,
+            logged_in: true
+        });
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.put('/edit/:id', withAuth, (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id);
+
+        if (!postData) {
+            res.status(404).json({ message: "No post found with this id!"});
+            return;
+        } else {
+            const post = postData.get({ plain: true });
+
+            res.render('edit-post', {
+                post,
+                logged_in: true
+            });
+        };
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
